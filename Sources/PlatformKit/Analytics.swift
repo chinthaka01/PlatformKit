9dbb67b7220fa1456d0df7a9d7283d4af5a26fe5
chinthaka01 <chinthaka01@gmail.com>
2025-12-23 17:44:38 -0500

    Addign the implementation files.

---
+++ Sources/PlatformKit/Analytics.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  PlatformKit
+//
+//  Created by Chinthaka Perera on 12/23/25.
+//
+
+import Foundation
+
+public class AnalyticsImpl: Analytics {
+    public func track(event: String) {
+        
+    }
+}
--- Sources/PlatformKit/PlatformKit.swift
+++ Sources/PlatformKit/PlatformKit.swift
@@ -1,2 +1,71 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftUI
+import UIKit
+
+public protocol MicroFeature {
+    var id: String { get }
+    var title: String { get }
+    var tabIcon: UIImage { get }
+    var selectedTabIcon: UIImage { get }
+
+    func makeRootView() -> AnyView
+}
+
+public protocol FeatureFactory {
+    func makeFeature() -> MicroFeature
+}
+
+public protocol Analytics {
+    func track(event: String)
+}
+
+/**
+ *  APIs
+ */
+
+public protocol FeedFeatureAPI {
+    func fetchFeeds() async throws -> FeedDTO
+}
+
+public protocol FriendsFeatureAPI {
+    func fetchFeeds() async throws -> FriendsDTO
+}
+
+public protocol ProfileFeatureAPI {
+    func fetchFeeds() async throws -> ProfileDTO
+}
+
+/**
+ *  Dependencies
+ */
+
+public protocol FeedDependencies {
+    var feedAPI: FeedFeatureAPI { get }
+    var analytics: Analytics { get }
+}
+
+public protocol FriendsDependencies {
+    var friendsAPI: FriendsFeatureAPI { get }
+    var analytics: Analytics { get }
+}
+
+public protocol ProfileDependencies {
+    var profileAPI: ProfileFeatureAPI { get }
+    var analytics: Analytics { get }
+}
+
+/**
+ * DTOs
+ */
+
+public protocol FeedDTO {
+    
+}
+
+public protocol FriendsDTO {
+    
+}
+
+public protocol ProfileDTO {
+    
+}
